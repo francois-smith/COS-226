a01a0d86b85fb548e14368d347ac9f62b29875e5
Francois Smith <steamfaffa@gmail.com>
2022-09-29 17:05:41 +0200

    Simple main To Test Implemented

--- Practicals/Practical 4/Task 2/Main.java
+++ Practicals/Practical 4/Task 2/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String args[]){
-        
+    public static void main(String args[])
+    {
+        VotingStation vs = new VotingStation();
+        Marshal[] marshals = new Marshal[5];
+
+        for(int i = 0; i < 5; i++){
+            marshals[i] = new Marshal(vs);
+            marshals[i].start();
+        }
+
     }
 }
